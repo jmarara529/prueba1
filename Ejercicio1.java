a3da740210e7e4e35b0c1e0d190f500ffe1638e1
jmarara529 <jmarara529@g.educaand.es>
2023-10-04 10:19:38 +0200

    versión inicial

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -6,8 +6,27 @@ public class Ejercicio1 {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("introduce valor para a: ");
-        Scanner a = entrada.nextLine();
+        int a = entrada.nextInt();
 
-        System.out.println(a);
+        System.out.println("introduce valor para b :");
+        int b = entrada.nextInt();
+
+        int c = entrada.nextInt();
+
+        entrada.close();
+
+        System.out.println("operacion a: ");
+        int solua =  a*b + c*((3-a)/(2*b));
+        System.out.println(solua);
+
+        System.out.println("operacion b: ");
+        int solub = ((2+a*b)/4)^(c+2);
+        System.out.println(solub);
+
+        System.out.println("operacion c: ");
+        String soluc = "no se hacer la raiz cuadrada en java";
+        System.out.println(soluc);
+
+
     }
 }
--- Ejercicio3.java
+++ Ejercicio3.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        int pizza
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("numero de pizzas que se han encargado");
-        Scanner pizza = entrada.nextLine();
+        System.out.println("numero de pizzas que se han encargado: ");
+        int pizza = entrada.nextInt();
+
+        System.out.println("numero de amigos: ");
+        int amigos = entrada.nextInt();
+
+        System.out.println("numero de porciones por pizza: ");
+        int porcionesPizza = entrada.nextInt();
+
+        entrada.close();
+
+        int porcionesTotal = porcionesPizza * pizza;
+
+        int porcionesPersona = porcionesTotal/amigos;
+
+        System.out.println("para " + amigos + " amigos, tocaran a " + porcionesPersona + " porciones por persona al pedir " + pizza + " pizzas y cortarla en " + porcionesPizza + " porciones");
 
     }
 }
