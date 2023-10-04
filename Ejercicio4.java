66d482654c7bde5f3da0278e47fa36096d46e0c2
jmarara529 <jmarara529@g.educaand.es>
2023-10-04 11:04:34 +0200

    versión inicial

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("introduce tres numeros enteros: ");
+        int a = entrada.nextInt();
+        int b = entrada.nextInt();
+        int c = entrada.nextInt();
+
+        string maximo = (a>b) ? (a>c) ? a : (b>c) ? b : c ;
+        string minimo = (a<b ? (a<c) ? a : (b<c) ? b : c ;
+        String medio = (a<b && a>c) ? a : (b<a && b>c) ? b : c;
+
+        System.out.println("valor minimo :" + minimo);
+        System.out.println("valor maximo: " + maximo);
+        System.out.println("valor medio: " medio);
+    }
 }
