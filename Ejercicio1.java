a4ed3b332e373f503bd9ecfaa31640023b4b3a6b
jmarara529 <jmarara529@g.educaand.es>
2023-10-04 09:54:37 +0200

    versión inicial

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("introduce valor para a: ");
+        Scanner a = entrada.nextLine();
+
+        System.out.println(a);
+    }
 }
--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        int pizza
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("numero de pizzas que se han encargado");
+        Scanner pizza = entrada.nextLine();
+
+    }
 }
